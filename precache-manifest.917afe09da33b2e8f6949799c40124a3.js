501f7f66a1059d95a356abb5af40ede166bc07d2
seif <seifashraf@eplango.com>
2020-08-25 10:08:09 +0200

    Updates

---
+++ precache-manifest.917afe09da33b2e8f6949799c40124a3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43ca8e3ca490453faab888d84da5dc7",
+    "url": "/decorAndYou/index.html"
+  },
+  {
+    "revision": "c9940aa86a1330f5489a",
+    "url": "/decorAndYou/static/css/2.954f26e3.chunk.css"
+  },
+  {
+    "revision": "1c3c7779c87c7d0ab43b",
+    "url": "/decorAndYou/static/css/main.6e9c98b0.chunk.css"
+  },
+  {
+    "revision": "c9940aa86a1330f5489a",
+    "url": "/decorAndYou/static/js/2.901f2a5f.chunk.js"
+  },
+  {
+    "revision": "a9bdc4b7da9c32408749acece13c610e",
+    "url": "/decorAndYou/static/js/2.901f2a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3c7779c87c7d0ab43b",
+    "url": "/decorAndYou/static/js/main.291a6deb.chunk.js"
+  },
+  {
+    "revision": "14f27042b13986deb434",
+    "url": "/decorAndYou/static/js/runtime-main.614c1479.js"
+  },
+  {
+    "revision": "fa34dbb5eb7d644c334847a6d62f74e4",
+    "url": "/decorAndYou/static/media/aboutHero.fa34dbb5.png"
+  },
+  {
+    "revision": "dd6026aabd66472fdc479fa4117598dc",
+    "url": "/decorAndYou/static/media/aboutImg.dd6026aa.png"
+  },
+  {
+    "revision": "f985567f7320e516e3e0295457869c20",
+    "url": "/decorAndYou/static/media/contactHero.f985567f.png"
+  },
+  {
+    "revision": "2c9f9f460ead86d97754352015552adf",
+    "url": "/decorAndYou/static/media/decor1.2c9f9f46.jpg"
+  },
+  {
+    "revision": "167843aebb95f71fc7651b7fdc601c86",
+    "url": "/decorAndYou/static/media/decor2.167843ae.jpg"
+  },
+  {
+    "revision": "8a13cc875f264b6cf7ce353053fffc55",
+    "url": "/decorAndYou/static/media/decor3.8a13cc87.jpg"
+  },
+  {
+    "revision": "b6d00b2984e0f2dab96dbab9e892f511",
+    "url": "/decorAndYou/static/media/decor4.b6d00b29.jpg"
+  },
+  {
+    "revision": "8e0b6dfcf0c1e4838ae0dbae63bd7d52",
+    "url": "/decorAndYou/static/media/decor5.8e0b6dfc.jpg"
+  },
+  {
+    "revision": "c5deb9be577e1aea3ea9a354f63a51d1",
+    "url": "/decorAndYou/static/media/decor6.c5deb9be.jpg"
+  },
+  {
+    "revision": "bdd2a9faadd688801ba4723b0e4d0b11",
+    "url": "/decorAndYou/static/media/decor7.bdd2a9fa.jpg"
+  },
+  {
+    "revision": "f7bc79f42292180b625f060bf17c7b7a",
+    "url": "/decorAndYou/static/media/decor8.f7bc79f4.jpg"
+  },
+  {
+    "revision": "258e51c27165b1698fc6503c1516a8d6",
+    "url": "/decorAndYou/static/media/galleryHero.258e51c2.png"
+  },
+  {
+    "revision": "ce1901dd31099a0d88ad653d509369a7",
+    "url": "/decorAndYou/static/media/grains.ce1901dd.png"
+  },
+  {
+    "revision": "382589495aed8d267af2398cdee54be4",
+    "url": "/decorAndYou/static/media/hero3.38258949.png"
+  },
+  {
+    "revision": "4670fcee5d32cacf818fbf0a7182e39d",
+    "url": "/decorAndYou/static/media/servicesHero.4670fcee.png"
+  },
+  {
+    "revision": "5ed2eb1ab011141381c74db0da853583",
+    "url": "/decorAndYou/static/media/team1.5ed2eb1a.jpg"
+  },
+  {
+    "revision": "0f810195b5bf8f0f48a15b18faba9176",
+    "url": "/decorAndYou/static/media/team2.0f810195.jpg"
+  },
+  {
+    "revision": "4ca8c58928cb1df8184e702485537634",
+    "url": "/decorAndYou/static/media/team3.4ca8c589.jpg"
+  },
+  {
+    "revision": "3dcd4649739216ffed36dc22c572c49c",
+    "url": "/decorAndYou/static/media/team4.3dcd4649.jpg"
+  },
+  {
+    "revision": "592788f47a6ff02d41b9defc0d9784e6",
+    "url": "/decorAndYou/static/media/team5.592788f4.jpg"
+  }
+]);
