1503405c4cf0fa8d25ed04e5d231a22e3f453514
seif <seifashraf@eplango.com>
2020-08-25 10:14:51 +0200

    firstDeployment

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         <ScrollViewPort />
 
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/decorAndYou" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/services" component={Services} />
           <Route path="/gallery" component={Gallery} />
