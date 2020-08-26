7e153583b308d1c56caf8dd5fa90b6d3b0a732fb
seif <seifashraf@eplango.com>
2020-08-26 12:01:05 +0200

    finalDeployment

--- src/components/servicesComponents/servicesWorkDemo.js
+++ src/components/servicesComponents/servicesWorkDemo.js
@@ -120,7 +120,7 @@ const ServicesWorkDemo = () => {
                             </OwlCarousel>
                         </div>
                         <div className="col-md-12 servicesWorkDemo-button">
-                            <Link to="./#/gallery" class="subscribe-btn">
+                            <Link to="./gallery" class="subscribe-btn">
                                 <button class="btn btn-theme" type="submit">View All Projects</button>
                             </Link>
                         </div>
